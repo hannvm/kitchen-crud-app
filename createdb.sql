c94de10e014a572cbd99dac507b69abfb39811b7
Hannah Minshull <hannahminshull@Hannahs-MacBook-Air.local>
2022-11-03 17:09:10 -0700

    db connected and data pulled

--- createdb.sql
+++ createdb.sql
@@ -2,12 +2,16 @@ CREATE TABLE ingredients (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     food varchar(100),
     qty int,
-    measurement = varchar(20),
+    measurement varchar(20),
 );
 
 CREATE TABLE recipes (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     recipe varchar(100),
-    ingredients varchar(255),
-    measurement = varchar(20),
+);
+
+CREATE TABLE pizza (
+    ingredient_id int NOT NULL PRIMARY KEY,
+    ingredient varchar(100),
+    qty int,
 );
--- script.php
+++ script.php
@@ -1,15 +1,31 @@
 <?php
-    /*
-    Exercise in OOP programming in PHP for kitchen management
-    */
 
+    //db connection
+    $host='localhost';
+    $dbName='kitchen-app';
+    $user='hannah';
+    $pass='insecure';
+
+    //DSN - data source name (string with associated data structure to describe db connection source)
+    $dsn= 'mysql:host=' . $host . ';dbname=' . $dbName;
+    $pdo= new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+    $stmt_get_ingredients = $pdo->query('SELECT * FROM ingredients');
+    $stmt_get_recipes = $pdo->query('SELECT * FROM recipes');
+
+
+
+    //classes
     class Food {
+        private $id;
         private $food;
         private $qty;
         private $meausrement;
         private $is_low;
 
-        public function __construct($food, $qty=0, $measurement, $is_low) {
+        public function __construct($id, $food, $qty=0, $measurement, $is_low) {
+            $this-> id = $id;
             $this-> food = $food;
             $this-> qty = $qty;
             $this-> measurement = $measurement;
@@ -22,27 +38,51 @@
     }
 
     class Recipe {
-        public $recipe;
-        public $ingredients;
+        private $id;
+        private $recipe;
 
-        public function __construct($recipe, $ingredients) {
+        public function __construct($id, $recipe) {
+            $this-> id = $id;
             $this-> recipe = $recipe;
-            $this-> ingredients = $ingredients;
-        }
-
-        public function getIngredients() {
-            return $this-> ingredients;
         }
     }
 
-    //ingredients
-    $marinara = new Food('marinara', 1000, 'grams', 'no');
-    $spaghetti = new Food('spaghetti', 1000, 'grams', 'no');
-    $mince = new Food('mince', 1000, 'grams', 'no');
-    $pizza_base = new Food('pizza_base', 3, 'count', 'no');
-    $mozarella = new Food('mozarella', 1000, 'grams', 'no');
+    //get array with all foods objects
+    while($row = $stmt_get_ingredients->fetch()) {
+        $array_ingredients[$row->food] = new Food($row->id, $row->food, $row->qty, $row->measurement, $row->is_low);
+    }       //print_r($array_ingredients);
 
-    //recipes
+    //get array with all recipes objects
+    while($row = $stmt_get_recipes->fetch()) {
+        $array_recipes[$row->recipe] = new Recipe($row->id, $row->recipe);
+    }       //print_r($array_recipes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // //ingredients
+    // $marinara = new Food('marinara', 1000, 'grams', 'no');
+    // $spaghetti = new Food('spaghetti', 1000, 'grams', 'no');
+    // $mince = new Food('mince', 1000, 'grams', 'no');
+    // $pizza_base = new Food('pizza_base', 3, 'count', 'no');
+    // $mozarella = new Food('mozarella', 1000, 'grams', 'no');
+
+    // //recipes
     $bolognese = new Recipe('bolognese', ['spaghetti' => '400', 'marinara' => '800', 'mince' => '500']);
 
 
